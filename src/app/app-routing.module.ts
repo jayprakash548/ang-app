8823b83dd701b21bf38a022f910ebbb1fd540695
Jay Prakash Kumar <jay.kasyap@live.com>
2018-11-16 04:28:19 +0530

    Created Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import { BlogViewComponent } from './blog-view/blog-view.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'about', component: AboutComponent },
+  { path: 'blog/:blogId', component: BlogViewComponent },
+  { path: 'create', component: BlogCreateComponent },
+  { path: 'edit/:editId', component: BlogEditComponent },
+  { path: '*', component: NotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [
+  HomeComponent,
+  AboutComponent,
+  BlogViewComponent,
+  BlogCreateComponent,
+  BlogEditComponent,
+  NotfoundComponent
+];
